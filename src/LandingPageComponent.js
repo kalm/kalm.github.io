d9c7909e6847674b5f7b381ee60a7d5c190de309
Madeleine Alzamora <zmadeleine12@gmail.com>
2017-07-20 00:09:20 -0400

    VideoComponent version 2

--- src/LandingPageComponent.js
+++ src/LandingPageComponent.js
@@ -8,6 +8,7 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
 import VideoComponent from './VideoComponent';
+import VideoComponentV2 from './VideoComponentV2';
 import CodeComponent from './CodeComponent';
 
 /* Local variables -----------------------------------------------------------*/
@@ -23,7 +24,7 @@ class LandingPageComponent extends Component {
 	render() {
 		return (
 			<div>
-				<VideoComponent />
+				<VideoComponentV2 />
 				<CodeComponent />
 			</div>
 		);
