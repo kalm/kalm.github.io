93da8793f95a13fe2a3aaa4bc1fb6da038679438
Frederic Charette <fcharette@premiumbeat.com>
2017-07-20 12:21:17 -0400

    proper branch for dev/deploy of user page

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+SOURCE_BRANCH="dev"
+TARGET_BRANCH="master"
 
 function doCompile {
   npm run build
