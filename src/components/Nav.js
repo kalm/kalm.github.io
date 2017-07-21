c5020cd39e063ec64901e956cce8bac8eb94f8e0
Bachir <bafosth@yahoo.fr>
2017-07-21 15:29:15 -0400

    Adding Navbar files

---
+++ src/components/Nav.js
@@ -0,0 +1,60 @@
+/**
+ * Version component
+ * - Displays the version number
+ */
+
+/* Imports -------------------------------------------------------------------*/
+
+import Inferno from 'inferno';
+import Component from 'inferno-component';
+
+/* Local variables -----------------------------------------------------------*/
+
+const version = '^3.0.0';
+
+/* Component -----------------------------------------------------------------*/
+
+class VersionComponent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	showVersion() {
+		alert(`The version is: ${ version }!`);
+	}
+
+	render() {
+		return (
+			<div class="container">
+			<nav class="navbar navbar-toggleable-md navbar-fixed-top navbar-light bg-faded">
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class="navbar-brand" href="index.html"><img src="./image/logo.png" alt="logo" class="logo"></a>
+
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Documentation</a>
+						</li>
+						<li class="nav-item">
+						<a class="nav-link" href="#">Disabled</a>
+						</li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="text" placeholder="Search">
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form>
+				</div>
+			</nav>
+		</div><!--end of header container-->
+		);
+	}
+}
+
+/* Exports -------------------------------------------------------------------*/ 
+
+export default VersionComponent;
