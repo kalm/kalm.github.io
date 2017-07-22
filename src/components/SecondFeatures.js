3d9ed5c5b8c2a631073289a318c7c0b1da5e3fdf
Bachir <bafosth@yahoo.fr>
2017-07-22 12:38:12 -0400

    Adding an alternative design for features

---
+++ src/components/SecondFeatures.js
@@ -0,0 +1,75 @@
+/**
+ * Second Features component
+ * This is another(the same but different design) features and some information
+ */
+
+/* Imports -------------------------------------------------------------------*/
+
+import Inferno from 'inferno';
+import Component from 'inferno-component';
+
+/* Local variables -----------------------------------------------------------*/
+
+const version = '^3.0.0';
+
+/* Component -----------------------------------------------------------------*/
+
+class VersionComponent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	showVersion() {
+		alert(`The version is: ${ version }!`);
+	}
+
+	render() {
+		return (
+			<div class="container features">
+				<div class="row">
+			<div class="col-lg-6 select1">
+			<ul>
+				<h3>Network Optimizations</h3>
+				<li> Scaling is more predictable</li>
+				<li> Atomic congestion control settings</li>
+				<li> Reduces bandwidth from protocol overhead</li>
+			</ul>
+			<img class="rounded-circle feat-img-1" src="./image/optimize.png" alt="Generic placeholder image">
+		</div>
+		<div class="col-lg-6 select2">
+          <img class="rounded-circle feat-img-2" src="./image/simple.png" alt="Generic placeholder image ">
+         
+          <ul>
+			<h3>Simple usage</h3>
+			<li> Intuitive single syntax for all protocols</li>
+			<li> Feature parity for all protocols for multiplexing</li>
+			<li> Stats and monitoring-enabled</li>
+          </ul>
+        </div>
+		<div class="col-lg-6 select3">
+			<ul>
+				<h3>Performances</h3>
+				<li> Raw throughput up to 8x higher </li>
+				<li> Median latency </li>
+				<li> Bandwidth </li>
+			</ul>
+			<img class="rounded-circle feat-img-3" src="./image/perform.png" alt="Generic placeholder image " >
+		</div>
+		<div class="col-lg-6 select4">
+			<img class="rounded-circle feat-img-4" src="./image/pressure.png" alt="Generic placeholder image">
+			<ul>
+				<h3>Hardware pressure</h3>
+				<li> (When running 10000 users in a typical scenario)</li>
+				<li> CPU usage is ~15% lower</li>
+				<li> RAM is ~10% lower</li>
+			</ul>
+		</div>
+		</div>
+			</div>
+		);
+	}
+}
+
+/* Exports -------------------------------------------------------------------*/ 
+
+export default VersionComponent;
