f30d7f6171a1542ad38771e4f9b79e75c3092da4
Bachir <bafosth@yahoo.fr>
2017-07-28 10:58:10 -0400

    Import css files to the different components

--- src/components/Features.js
+++ src/components/Features.js
@@ -7,6 +7,7 @@
 
 import Inferno from 'inferno';
 import Component from 'inferno-component';
+import style from 'style.js';
 
 /* Component -----------------------------------------------------------------*/
 
