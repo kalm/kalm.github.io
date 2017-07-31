438b2805b57c3e6828a06c43256c6a5ee23fe0c2
Madeleine Alzamora <zmadeleine12@gmail.com>
2017-07-30 23:55:40 -0400

    error fixed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,7 +7,6 @@
 
 import Inferno from 'inferno'; // eslint-disable-line
 import VideoComponent from '../components/VideoComponent';
-import VideoComponentV2 from '../components/VideoComponentV2';
 import CodeComponent from '../components/CodeComponent';
 
 /* Exports -------------------------------------------------------------------*/
