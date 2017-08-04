e643cd27a00a31b4ec80a362d6e47f4d0a4d5d05
Madeleine Alzamora <zmadeleine12@gmail.com>
2017-08-04 08:53:40 -0400

    temp stuff

--- webpack.conf.js
+++ webpack.conf.js
@@ -18,7 +18,7 @@ module.exports = {
 			{
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
-            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+            'file-loader?hash=sha512&digest=hex&name=[name].[ext]',
             'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
         ]
     	}
