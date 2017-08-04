9e0d6b56e55bf8c0c16b702fc03660331406e333
Madeleine Alzamora <zmadeleine12@gmail.com>
2017-08-04 11:16:51 -0400

    working in progress codeComponent

--- src/components/CodeComponent.js
+++ src/components/CodeComponent.js
@@ -15,8 +15,11 @@ class CodeComponent extends Component {
 			<div className="stripe-1">
 				<div className="container pt-5">
 					<div className="row">
-						<div className="col-md-12">
-							<h2 className="section-title text-center pb-4 my-5">Getting started</h2>
+						<div className="col-md-12 text-center">
+							<h2 className="section-title pb-3 mt-5">Getting started</h2>
+								<p className="py-3 section-subtitle">
+									Lorem ipsum dolor sit amet, consectetur<br hidden-sm-down /> adipisicing elit,
+				sed do eiusmod tempor incididunt ut labore.</p>
 						</div>
 
 						<div className="col-md-6 mt-5">
@@ -28,8 +31,9 @@ class CodeComponent extends Component {
 						</div>
 
 						<div className="col-md-6 text-center my-5">
-							<h4 className="description-btn">Setting up your server</h4>
-							<button type="button" className="btn btn-default btn-doc mb-5">Documentation</button>
+							<h4 className="description-btn">Server</h4>
+							<h3 class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</h3>
+							<button type="button" className="btn btn-default btn-doc mb-5">Getting started</button>
 						</div>
 
 					</div>
@@ -38,7 +42,8 @@ class CodeComponent extends Component {
 				<div className="container pt-5">
 					<div className="row">
 						<div className="col-md-6 text-center my-5">
-							<h4 className="description-btn">Setting up your server</h4>
+							<h4 className="description-btn">Client</h4>
+							<h3 class="section-subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</h3>
 							<button type="button" className="btn btn-default btn-doc mb-5">Documentation</button>
 						</div>
 
