3264d0507ddd88ff3a8a7269d254f8f8904663a4
Bachir <bafosth@yahoo.fr>
2017-09-01 11:29:08 -0400

    Made some corrections to the features component

--- src/components/Features.js
+++ src/components/Features.js
@@ -18,44 +18,57 @@ class Features extends Component {
 			<div className="stripe-1 z-top">
 				<div className="container features spacing-1x">
 					<div className="row">
-						<div className="col-lg-6">
-							<img className="rounded-circle feature-img" src="./images/optimize.png" alt="Generic placeholder image" />
-							<ul>
+						<div className="media col-lg-6">
+							<img className="d-flex align-self-start mr-3" src="./images/optimize.png" alt="Generic placeholder image" />
+							<div className="media-body">
 								<h3>Network Optimizations</h3>
-								<li>- Scaling is more predictable</li>
-								<li>- Atomic congestion control settings</li>
-								<li>- Reduces bandwidth from protocol overhead</li>
-							</ul>
+								<ul>
+									<li>- Scaling is more predictable</li>
+									<li>- Atomic congestion control settings</li>
+									<li>- Reduces bandwidth from protocol overhead</li>
+								</ul>
+							</div>			
 						</div>
-						<div className="col-lg-6">
-							<img className="rounded-circle feature-img" src="./images/use.png" alt="Generic placeholder image" />
-							<ul>
-								<h3>Simple usage</h3>
-								<li>- Intuitive single syntax for all protocols</li>
-								<li>- Feature parity for all protocols for multiplexing</li>
-								<li>- Stats and monitoring-enabled</li>
-							</ul>
 
+						<div className="media col-lg-6">
+							<img className="d-flex align-self-start mr-3" src="./images/use.png" alt="Generic placeholder image" />
+							<div className="media-body">
+								<h3>Simple usage</h3>
+								<ul>
+									<li>- Intuitive single syntax for all protocols</li>
+									<li>- Feature parity for all protocols for multiplexing</li>
+									<li>- Stats and monitoring-enabled</li>
+								</ul>
+							</div>			
 						</div>
-						<div className="col-lg-6">
-							<img className="rounded-circle feature-img" src="./images/perform.png" alt="Generic placeholder image" />
-							<ul>
+					</div>	
+
+					<div className="row">								
+						<div className="media col-lg-6">
+							<img className="d-flex align-self-start mr-3" src="./images/perform.png" alt="Generic placeholder image" />
+							<div className="media-body">
 								<h3>Performances</h3>
-								<li>- Raw throughput up to 8x higher ( graphics will be included)</li>
-								<li>- Median latency (graphics)</li>
-								<li>- Bandwidth (graphics)</li>
-							</ul>
+								<ul>
+									<li>- Raw throughput up to 8x higher</li>
+									<li>- Median latency (graphics)</li>
+									<li>- Bandwidth (graphics)</li>
+								</ul>
+							</div>			
 						</div>
-						<div className="col-lg-6">
-							<img className="rounded-circle feature-img" src="./images/pressure.png" alt="Generic placeholder image" />
-							<ul>
+
+						<div className="media col-lg-6">
+							<img className="d-flex align-self-start mr-3" src="./images/pressure.png" alt="Generic placeholder image" />
+							<div className="media-body">
 								<h3>Hardware pressure</h3>
-								<li>- (When running 10000 users in a typical scenario)</li>
-								<li>- CPU usage is ~15% lower</li>
-								<li>- RAM is ~10% lower</li>
-							</ul>
-						</div>
+								<ul>
+									<li>- When running 10000 users in a typical scenario</li>
+									<li>- CPU usage is ~15% lower</li>
+									<li>- RAM is ~10% lower</li>
+								</ul>
+							</div>			
+						</div>						
 					</div>
+
 				</div>
 			</div>
 		);
